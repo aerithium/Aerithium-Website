b38214c284f1cdfd1f387f326e0631da96812284
DESKTOP-HGI3B5A\yoric <yorick@vanderriet.net>
2019-02-25 02:39:07 +0100

    Updated database model

--- Aerithium/Models/AerithiumDBModel.Designer.cs
+++ Aerithium/Models/AerithiumDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\yoric\onedrive\documenten\visual studio 2017\Projects\Aerithium\Aerithium\Models\AerithiumDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\yoric\OneDrive\Documenten\Visual Studio 2017\Projects\Aerithium-Website\Aerithium\Models\AerithiumDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
